173a083e04c1c488faae5688fd867ca9c5c36972
callas4ever <thetseringnorbu@gmail.com>
2020-07-13 19:06:49 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { pattern1 } from './data.js'
 import Matrix from './Matrix.js'
-
+this.state={
+    values: pattern1
+}
 ReactDOM.render(<Matrix values={pattern1} />, document.getElementById('root'));
